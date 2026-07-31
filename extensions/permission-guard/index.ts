7ed852ee90b177c7b0714f795cec45ab5cf0f516
j0k3r.home <j0k3r.dev.rgl@gmail.com>
2026-07-31 09:35:51 -0300

    chore: remove permission guard and simplify agent config

--- extensions/permission-guard/index.ts
+++
@@ -1,5 +0,0 @@
-import { registerPermissionGuardRuntime, type RuntimePiLike } from './src/runtime.js';
-
-export default function permissionGuardExtension(pi: RuntimePiLike): void {
-  registerPermissionGuardRuntime(pi);
-}
